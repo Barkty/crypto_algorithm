4e4dbc8d3516ca02aaeadb69d88c7841a7003f74
Kotoye Gbemisola <58271499+Barkty@users.noreply.github.com>
2022-02-20 20:09:23 +0100

    problem solved

--- sales-commission/index.js
+++ sales-commission/index.js
@@ -29,5 +29,22 @@ const handleSubmit = () => {
     var amount3 = document.getElementById('price').value;
 
     console.log(amount1, amount2, amount3);
-    
+
+    let amt1 = parseFloat(amount1)
+    let amt2 = parseFloat(amount2)
+    let amt3 = parseFloat(amount3)
+
+    //Initial Salary
+    var initialSalary = 200;
+
+    //Total of sales
+    var sales = amt1 + amt2 + amt3;
+
+    //percentage of sales
+    var per = parseInt((9 / 100) * sales);
+
+    let totalSalary = initialSalary + per;
+    console.log(totalSalary);
+    var out = document.getElementById('lu');
+    out.innerHTML = `Total salary for the month is ${totalSalary}`;
 }
