4688468221f4c08bd9f14c805eb47e12f3595c2c
Kotoye Gbemisola <58271499+Barkty@users.noreply.github.com>
2022-02-22 08:27:00 +0100

    update gas mileage

--- sales-commission/index.js
+++ sales-commission/index.js
@@ -24,27 +24,34 @@ const addNewRow = () => {
 }
 
 const handleSubmit = () => {
-    var amount1 = document.getElementById('amount').value;
-    var amount2 = document.getElementById('amt').value;
-    var amount3 = document.getElementById('price').value;
+    var mile1 = document.getElementById('mile1').value;
+    var mile2 = document.getElementById('mile2').value;
+    var mile3 = document.getElementById('mile3').value;
+    
+    var gas1 = document.getElementById('gallon1').value;
+    var gas2 = document.getElementById('gallon2').value;
+    var gas3 = document.getElementById('gallon3').value;
+    console.log(mile1, mile2, mile3);
+    console.log(gas1, gas2, gas3);
 
-    console.log(amount1, amount2, amount3);
+    let amt1 = parseFloat(mile1)
+    let amt2 = parseFloat(mile2)
+    let amt3 = parseFloat(mile3);
 
-    let amt1 = parseFloat(amount1)
-    let amt2 = parseFloat(amount2)
-    let amt3 = parseFloat(amount3)
+    let gal1 = parseFloat(gas1)
+    let gal2 = parseFloat(gas2)
+    let gal3 = parseFloat(gas3);
 
-    //Initial Salary
-    var initialSalary = 200;
+    //Calculate mileage
+    var mileage1 = gal1 / amt1;
+    var mileage2 = gal2 / amt2;
+    var mileage3 = gal3 / amt3;
 
-    //Total of sales
-    var sales = amt1 + amt2 + amt3;
-
-    //percentage of sales
-    var per = parseInt((9 / 100) * sales);
-
-    let totalSalary = initialSalary + per;
-    console.log(totalSalary);
+    var totalMileage = mileage1 + mileage2 + mileage3;
+    console.log(mileage1);
+    console.log(mileage2);
+    console.log(mileage3);
+    console.log(totalMileage);
     var out = document.getElementById('lu');
-    out.innerHTML = `Total salary for the month is ${totalSalary}`;
+    out.innerHTML = `First mileage is ${mileage1}. <br> Second mileage is ${mileage2}. <br>Third mileage is ${mileage3}. <br> Total mileage is ${totalMileage}`;
 }
