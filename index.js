b94521e2253df2fe29fea6f7facab738d93a4013
Kotoye Gbemisola <58271499+Barkty@users.noreply.github.com>
2022-02-28 14:16:21 +0100

    salary calculator updated

--- index.js
+++ index.js
@@ -1,73 +1,46 @@
-var store = window.localStorage.getItem('encrypt');
-
-const load = () => {
-    if (store != null) {
-        
-    }
+const addNewRow = () => {
+    let row = document.createElement('tr');
+    //console.log(row);
+    let text = `<tr>
+                    <td>
+                        <div>
+                            <input type="text" name="char" class="char" id="item">
+                        </div>
+                    </td>
+                    <td>
+                        <div>
+                            <input type="text" name="char" class="char" id="char">
+                        </div>
+                    </td>
+                </tr>`;
+    //console.log(text);
+    row.innerHTML = text;
+    //row.appendChild(text);
+    //console.log(row);            
+    let form = document.querySelector('#salesForm');
+    form.innerHTML += text;
+    //form.innerHTML = row;
+    console.log(form);
 }
 
-const charToInt = () => {
-    var input = document.getElementById('char').value;
-    var newInput = parseInt(input);
-    //console.log(newInput);
-    var str = newInput.toString();
-    //console.log(str);
-    var out = []
-    
-    for (let i = 0; i < str.length; i++) {
-        out.push((+str.charAt(i) + 7) % 10);
-    }
-    console.log(out); //logs [8, 9, 0, 1]
-    const swapNum = (arr, indexA, indexB) => {
-        var temp = arr[indexA];
-        arr[indexA] = arr[indexB];
-        arr[indexB] = temp;
-    }
-    swapNum(out, 0, 2);
-    swapNum(out, 1, 3);
-    console.log(out);
-    let res = Number(out.join(''));
-    console.log(res); // logs 0189
+const handleSubmit = () => {
+    let ratePerHour = document.getElementById('amount').value;
+    let hoursOfWork = document.getElementById('hour').value;
 
-    var ul = document.getElementById('ul');
-    ul.innerHTML = `The encrpyted integer is ${res}`;
-}
+    console.log(hoursOfWork);
+    console.log(ratePerHour);
 
-const decrypt = () => {
-    var input = document.getElementById('dec').value;
-    var newInput = parseInt(input);
-    var str = newInput.toString();
-    var out = [];
-    let res = [];
-    let sub = [];
-    let int = [];
-    for (let i = 0; i < str.length; i++) {
-        out.push(+str.charAt(i));
-    }
-    const swapNum = (arr, indexA, indexB) => {
-        var temp = arr[indexA];
-        arr[indexA] = arr[indexB];
-        arr[indexB] = temp;
-    }
-    swapNum(out, 0, 2);
-    swapNum(out, 1, 3);
+    let hr1 = parseFloat(hoursOfWork);
+    let rates = parseFloat(ratePerHour);
+    let out = document.getElementById('lu');
 
-    for (let i = 0; i < out.length; i++){
-        res.push((10 * 1) + (+out[i]))
-    }
-    for (let i = 0; i < res.length; i++){
-        sub.push(+res[i] - 7);
-    }
+    if (hr1 > 40) {
+        let extraHours = hr1 - 40;
+        let halfTime = extraHours / 2;
+        let salary = (40 * rates) + (halfTime * rates);
 
-    for (let i = 0; i < sub.length; i++){
-        if(sub[i] > 9) {
-            int.push(+sub[i] - 10);
-        } else {
-            int.push(+sub[i]);
-        }
+        out.innerHTML = `Total salary for the month is ${salary}`;
+    } else {
+        out.innerHTML = `Total salary for the month is ${hr1 * rates}`;
     }
-
-    let ner = Number(int.join(''));
-    var lu = document.getElementById('lu');
-    lu.innerHTML = `The decrpyted integer is ${ner}`;
 }
